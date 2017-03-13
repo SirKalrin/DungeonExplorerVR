938f42e6c4a9cd027f6af60b8925819dfbe8cebd
BilleTheBug <dr.iversen@hotmail.com>
2017-03-13 23:17:39 +0100

    Menu Added
    Menu-scene added where you can go to all members test scenes by
    activating the first letter in their name.

---
+++ Assets/Scripts/MenuItemMgr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuItemMgr : MonoBehaviour
+{
+    public Light LightSource;
+
+    public void BClicked()
+    {
+        SceneManager.LoadScene("BilleTestScene");
+    }
+    public void MClicked()
+    {
+        SceneManager.LoadScene("MortensTestScene");
+    }
+    public void NClicked()
+    {
+        SceneManager.LoadScene("NicolaiTestScene");
+    }
+
+    public void PointerEntered()
+    {
+        LightSource.enabled = true;
+        transform.position = new Vector3(transform.position.x + 0.2f, transform.position.y, transform.position.z);
+    }
+    public void PointerExited()
+    {
+        LightSource.enabled = false;
+        transform.position = new Vector3(transform.position.x - 0.2f, transform.position.y, transform.position.z);
+    }
+
+}
