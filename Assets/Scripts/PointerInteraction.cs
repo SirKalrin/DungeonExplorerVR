3a3da2ff6ffe7f637d2cce7046ed81ce0aaf04f0
BilleTheBug <dr.iversen@hotmail.com>
2017-03-15 10:37:04 +0100

    Player prefab added
    Added a static player prefab.

---
+++ Assets/Scripts/PointerInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerInteraction : MonoBehaviour {
+    [SerializeField]
+    private GvrReticlePointer _pointer;
+    public void PointerEntered()
+    {
+        _pointer.GetComponent<MeshRenderer>().enabled = true;
+    }
+    public void PointerExited()
+    {
+        _pointer.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void Test()
+    {
+        Debug.Log("Test");
+    }
+
+}
