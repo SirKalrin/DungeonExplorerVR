78e601dc9ef534891aed74c7ae5edfffcb9a6bcd
NicolaiMensel <nicolai.m.jorgensen@gmail.com>
2017-03-28 16:05:08 +0200

    A simple and uncomplete ombat system have been implemented. To be finished

---
+++ Assets/Scripts/AIScripts/AIStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIStats : MonoBehaviour
+{
+
+    public int Level = 1;
+    public int Health;
+
+    public int Damage;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Health = 100*Level;
+	    Damage = 20*Level;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/CombatManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AttackTarget(GameObject attacker, GameObject target)
+    {
+        if (attacker.tag != "Player")
+        {
+            AIStats attackerStats = attacker.GetComponent<AIStats>();
+            PlayerStats targetStats = target.GetComponent<PlayerStats>();
+
+            targetStats.Health -= attackerStats.Damage * (attackerStats.Level / targetStats.Level);
+
+            Debug.Log("Player health: " + targetStats.Health);
+        }
+    }
+}
