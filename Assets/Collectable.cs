ba6ceedbd8c4c2b45ccbddb198342272dad6d789
BilleTheBug <dr.iversen@hotmail.com>
2017-03-28 22:19:45 +0200

    WS first steps
    Map created and painted, water added, new script for picking up arrows
    added.

---
+++ Assets/Collectable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : PointerInteraction
+{
+    public void QuiverPickup()
+    {
+        
+    }
+}
